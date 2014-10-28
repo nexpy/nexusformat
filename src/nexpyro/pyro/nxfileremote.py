7e1108f94d317a715494e1ccd0c050840cd1ca13
Ray Osborn <rayosborn@mac.com>
2014-10-28 16:52:58 -0500

    Added _readvalue and read file
    ‘_readvalue’ calls the nxfileservice getvalue function with both a path
    and a key. ‘read file’ loads the whole root tree, as in the parent
    NXFile class.

--- src/nexpyro/pyro/nxfileremote.py
+++ src/nexpyro/pyro/nxfileremote.py
@@ -8,8 +8,9 @@ Contains a Pyro proxy
 import sys
 import Pyro4
 
-import nexpy.api.nexus as nx
-from nexpy.api.nexus import NXFile
+import nexpyro.nexus as nx
+from nexpyro.nexus import NXFile
+from nexpy.api.nexus import NXfield
 
 import numpy as np
 
@@ -18,18 +19,22 @@ def message(msg):
 
 class NXFileRemote(NXFile):
 
-    def __init__(self, uri, name):
+    def __init__(self, name, uri):
         # Get a Pyro proxy to the remote object
         Pyro4.config.SERIALIZER = "pickle"
         message("proxy connect")
         proxy = Pyro4.Proxy(uri)
         message("proxy init")
+        proxy._pyroTimeout = 20.0
         b = proxy.initfile(name)
         if b != True:
             print "\nERROR OCCURRED IN SERVICE"
             print b
             return
         message("file init")
+        self._mode = 'r'
+        self._file = proxy
+        self._filename = name
         NXFile.__init__(self, name, proxy=proxy)
         assert(b)
 
@@ -41,3 +46,33 @@ class NXFileRemote(NXFile):
 
     def __exit__(self, *args):
         self._file.exit(0)
+
+    def _readvalue(self, path, key=None):
+        return self._file.getvalue(path, key)
+
+    def readfile(self):
+        tree = self._file.tree()
+        tree._file = self
+        return tree
+        
+
+def setserver(value):
+    global _global_server
+    _global_server = value
+    
+def getserver():
+    global _global_server
+    return _global_server 
+
+def nxloadremote(filename, uri):
+    """
+    Reads a NeXus file returning a tree of objects.
+
+    This is aliased to 'nxload' because of potential name clashes with Numpy
+    """
+    nxrfile = NXFileRemote(filename, uri)
+    tree = nxrfile.readfile()
+    print "nx.load done."
+    return tree
+
+

d97c25fce0f2ae5f8a4de21ee9d54f44ebcf8b7b
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-01-13 14:33:19 -0600

    Better error handling

--- src/nexusformat/pyro/ssh.py
+++ src/nexusformat/pyro/ssh.py
@@ -1,15 +1,18 @@
 
-import os
+import os, time
+import select
+import signal
+from select import POLLIN
 from multiprocessing import Process, Queue
 from subprocess import Popen, PIPE, STDOUT
 
 _ssh_process_id = 0
 
-class SSHProcess:
+class NeXpyroSSH:
 
     def __init__(self, user, host,
                  hours=1, localPort=0, remotePort=0,
-                 command=None):
+                 command=None, getURI=False):
 
         self.user = user
         self.host = host
@@ -29,40 +32,68 @@ class SSHProcess:
             duration = str(self.hours * 60 * 60)
             self.command = "sleep " + duration
 
-        q = Queue() 
-        self.process = Process(target=self.f, args=(q,))
+        self.queue = Queue()
+        self.process = Process(target=self.f, args=(self.queue,))
         self.process.start()
-        result = q.get(block=True, timeout=20)
-        self.msg("result: " + result)
-        if result.startswith("URI: "):
-            self.uri = result.split(" ")[1]
+        if getURI:
+            result = self.queue.get(block=True, timeout=20)
+            self.msg("result: " + result)
+            if result.startswith("URI: "):
+                self.uri = result.split(" ")[1]
 
     def account(self):
         return "%s@%s" % (self.user, self.host)
 
-    def f(self, q):
+    def f(self, queue):
         self.msg("PID: " + str(os.getpid()));
         self.msg("connecting " + self.command)
-        argv = ["ssh", self.account(), self.command]
-        p = Popen(argv, bufsize=0,
-                  stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-        # p.stdin.close()
+        argv = ["ssh", "-tt", self.account()]
+        if self.localPort != 0:
+            argv.append("-L %i:localhost:%i" % 
+                        (self.localPort, self.remotePort))
+        argv.append(self.command)
+        pipe = self.make_pipe(argv)
+        pipe.stdin.close()
+        poller = select.poll()
+        poller.register(pipe.stdout.fileno(), POLLIN)
         while True:
-            line = p.stdout.readline()
-            self.msg("read: " + line)
-            self.output += line
-            if line.startswith("URI: "):
-                print("found URI %s" % line)
-                q.put(line)
-                
-        exitcode = p.wait()
+            try:
+                L = poller.poll(500)
+            except KeyboardInterrupt:
+                self.msg("Interrupted!")
+            if len(L) > 0:
+                if L[0][1] != POLLIN: 
+                    self.msg("poll found error")
+                    break
+                line = pipe.stdout.readline()
+                self.msg("read: " + line.rstrip())
+                self.output += line
+                if queue != None and line.startswith("URI: "):
+                    print("found URI %s" % line)
+                    queue.put(line)
+            if not queue.empty():
+                item = queue.get()
+                if item == "TERMINATE":
+                    break
+        self.msg("terminating pipe")
+        pipe.terminate()
+        exitcode = pipe.wait()
         result = "exited with code: %i" % exitcode
         self.msg(result)
-        q.put(result)
+        queue.put(result)
+
+    def make_pipe(self, argv):
+        command = " ".join(argv)
+        self.msg("running: " + command)
+        return Popen(command, bufsize=0, 
+                     stdin=PIPE, stdout=PIPE, stderr=STDOUT, 
+                     close_fds=True, shell=True)
 
     def msg(self, m):
-        print "SSHProcess(%i) %s: %s" % (self.id, self.account(), str(m))
+        print "NeXpyroSSH(%i) %s: %s" % (self.id, self.account(), str(m))
 
     def terminate(self):
         self.msg("killing")
+        self.queue.put("TERMINATE")
+        time.sleep(1)
         self.process.terminate()
--- src/nexusformat/pyro/tests/TestSSHProcess.py
+++ src/nexusformat/pyro/tests/TestSSHProcess.py
@@ -1,19 +1,28 @@
 
-import time
-from nexusformat.pyro.ssh import SSHProcess
+import sys, time
+# import signal
+from nexusformat.pyro.ssh import NeXpyroSSH
+
+_terminated = False
 
 hostname = "nxrs.msd.anl.gov"
 hostname = "130.202.115.40"
 
-# p = SSHProcess("wozniak", hostname)
-# p = SSHProcess("wozniak", hostname, command="echo hi")
+# sshService = NeXpyroSSH("wozniak", hostname)
+# sshService = NeXpyroSSH("wozniak", hostname, command="echo hi")
 
 # start_server = "/usr/lib/python2.7/site-packages/nexusformat/pyro/start_server.py"
-start_server = "/home/wozniak/proj/nexusformat/src/nexusformat/pyro/start_server.py"
-command = "python " + start_server
-p = SSHProcess("wozniak", hostname, command=command)
-time.sleep(2)
-print("uri: " + p.uri)
-time.sleep(2)
-p.terminate()
-print("output was: " + p.output)
+command = "/home/wozniak/proj/nexusformat/src/nexusformat/pyro/start_server.sh"
+command = "/home/wozniak/proj/nexusformat/src/nexusformat/pyro/start_server.py"
+sshService = NeXpyroSSH("wozniak", hostname, command=command, getURI=True)
+uri = sshService.uri
+if (uri == "UNSET"):
+    print("SSH could not start NeXpyro service!")
+    sys.exit(1)
+print("uri: " + uri)
+tokens = uri.split(":")
+port = int(tokens[2])
+print(port)
+sshTunnel = NeXpyroSSH("wozniak", hostname, localPort=9090, remotePort=port)
+
+time.sleep(200)

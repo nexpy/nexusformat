4c0327e8e2d91234902138605e2f71205cca70ed
Ray Osborn <rayosborn@mac.com>
2016-02-08 10:57:14 -0600

    Add supported Python versions
    Automatically updates PyPI listing.

--- setup.py
+++ setup.py
@@ -48,6 +48,10 @@ setup (name =  nexusformat.__package_name__, # NeXpy
                      'Programming Language :: Python',
                      'Programming Language :: Python :: 2',
                      'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3.3',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
                      'Topic :: Scientific/Engineering',
                      'Topic :: Scientific/Engineering :: Visualization'],
       )

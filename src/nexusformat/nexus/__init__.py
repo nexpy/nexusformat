d864fdf8740c15285c2ad56a52327cea99cf8059
Ray Osborn <rayosborn@mac.com>
2017-03-07 09:58:21 -0600

    First commit of remote.py
    This subclasses NXFile in order to utilize h5pyd to access HDF5 files
    on a remote server running h5serv.

--- src/nexusformat/nexus/__init__.py
+++ src/nexusformat/nexus/__init__.py
@@ -68,3 +68,8 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from .tree import *
+try:
+    import h5pyd
+    from .remote import *
+except ImportError:
+    pass

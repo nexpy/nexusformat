366019c4546d62cdb0272ab376429d7cda4e0526
Ray Osborn <rayosborn@mac.com>
2017-09-07 17:50:39 -0500

    Fix regular expression to extract axes
    The old version generated a future warning, because it could return an empty string. This removes the warning, and adds code to strip surrounding brackets or parentheses.

--- src/nexusformat/nexus/tree.py
+++ src/nexusformat/nexus/tree.py
@@ -962,7 +962,7 @@ def _readaxes(axes):
     The delimiter separating each axis can be white space, a comma, or a colon.
     """
     if is_text(axes):
-        return list(re.split(r'[,:; \[\]]?', axes))
+        return list(re.split(r'[,:; ]', axes.strip('[]()')))
     else:
         return list(axes)
 

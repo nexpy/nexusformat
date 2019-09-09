c890638578948f3966412a9fc876a9d11430b271
Ray Osborn <rayosborn@mac.com>
2019-09-08 19:50:26 -0500

    Update lock tests to the NXFile changes

--- tests/test_locks.py
+++ tests/test_locks.py
@@ -8,14 +8,17 @@ field1 = NXfield('a', name="f1")
 
 def test_lock_creation(tmpdir):
 
-    filename = os.path.join(tmpdir, 'file1.nxs')
+    filename = os.path.join(tmpdir, "file1.nxs")
     root = NXroot(NXentry(field1))
     root.save(filename)
 
-    assert root.nxfile.lock is None
+    assert root.nxfile.lock is not None
 
-    root.nxfile.lock = 20
     root.nxfile.acquire_lock()
+    
+    assert not root.nxfile.locked
+    assert not root.nxfile.is_locked()
+
     root.nxfile.release_lock()
 
     assert not root.nxfile.locked
@@ -28,9 +31,9 @@ def test_lock_creation(tmpdir):
     assert root.nxfile.is_locked()
     assert root.nxfile.lock.timeout == 10
 
-    root['entry/f1'] = 'b'
+    root["entry/f1"] = "b"
 
-    assert root['entry/f1'] == 'b'
+    assert root["entry/f1"] == "b"
     
     assert not root.nxfile.locked
     assert not root.nxfile.is_locked()
@@ -38,20 +41,18 @@ def test_lock_creation(tmpdir):
 
 def test_locked_assignments(tmpdir):
 
-    filename = os.path.join(tmpdir, 'file1.nxs')
+    filename = os.path.join(tmpdir, "file1.nxs")
     root = NXroot(NXentry(field1))
     root.save(filename)
 
     assert root.nxfile.mtime == os.path.getmtime(filename)
-    assert root.nxfile.lock is None
 
     original_id = id(root.nxfile)
-    original_mtime = root._mtime
+    originalmtime = root.mtime
 
     root["entry/f1"] = "b"
 
     assert root["entry/f1"] == "b"
-    assert root.nxfile.lock is None
     
     root.nxfile.lock = 10
 
@@ -62,7 +63,7 @@ def test_locked_assignments(tmpdir):
 
     assert root["entry/f1"] == "c"
     assert id(root.nxfile) == original_id
-    assert root._mtime > original_mtime
+    assert root.mtime > originalmtime
 
 
 def test_lock_interactions(tmpdir):
@@ -75,19 +76,11 @@ def test_lock_interactions(tmpdir):
 
     root1 = nxload(filename, mode="rw")
     root2 = nxload(filename, mode="r")
-    root1_id = id(root1.nxfile)
-    root1_mtime = root1._mtime
-    root2_id = id(root2.nxfile)
-    root2_mtime = root2._mtime
-
-    assert root1.nxfile.lock is None
-    assert root2.nxfile.lock is None
 
     root1.nxfile.lock = 10
     root1["entry/f1"] = "b"
 
-    assert isinstance(root1.nxfile.lock, NXLock)
-    assert root1._mtime > root2._mtime
+    assert root1.mtime > root2.mtime
 
 
 def test_lock_defaults(tmpdir):  
@@ -111,6 +104,5 @@ def test_lock_defaults(tmpdir):
     root.save(filename, "w")
 
     with root.nxfile as f:
-        assert root.nxfile.lock is None
         assert not root.nxfile.locked
         assert not root.nxfile.is_locked()    

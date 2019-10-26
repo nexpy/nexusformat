ae7e47cf98b1f1986e62d1f452f48405cc235935
Ray Osborn <rayosborn@mac.com>
2019-10-26 14:43:01 -0500

    Add extra test to test_entry_group

--- tests/test_entry_groups.py
+++ tests/test_entry_groups.py
@@ -12,12 +12,14 @@ v.resize((2,5,10))
 def test_entry_operations():
 
     e1 = NXentry(NXdata(2*v, (z, y, x), name='d1'), NXsample(name='s1'))
+    e1.attrs['default'] = 'd1'
     e2 = NXentry(NXdata(v, (z, y, x), name='d1'))
     
     e3 = e1 + e2
 
     assert 'd1' in e3
     assert 's1' in e3
+    assert e3.attrs['default'] == 'd1'
     assert np.array_equal(e3.d1.nxsignal.nxvalue, 3*v)
     assert e3.d1.nxaxes == e1.d1.nxaxes
     assert e3.d1.nxsignal.nxname == "v"

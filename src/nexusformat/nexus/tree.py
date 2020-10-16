673ee364af3deaea1040a5f81f5ff67ec5de18e3
Ray Osborn <rayosborn@mac.com>
2020-10-16 14:26:10 -0500

    Stop search once plottable data has been found
    This prevents unnecessary loading of a file when the NXFile is defined with 'recursive=False'.

--- src/nexusformat/nexus/tree.py
+++ src/nexusformat/nexus/tree.py
@@ -4808,6 +4808,7 @@ class NXgroup(NXobject):
         for entry in self:
             if self[entry].is_plottable():
                 plottable = True
+                break
         return plottable        
 
     @property

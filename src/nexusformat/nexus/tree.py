Fix issue caused by np.sign(0.0) = 0.0
This prevents the remainder from always being 0 when the axis value is 0 when selecting data with a particular divisor and offset.

src/nexusformat/nexus/tree.py
@@ -6195,14 +6195,16 @@ class NXdata(NXgroup):
                                 np.remainder(x+offset,  divisor), 
                                        divisor, atol=tol))
         else:
+            def sign(x):
+                return np.where(x!=0.0, np.sign(x), 1)
             condition = np.where(
                             np.isclose(
                                 np.remainder(
-                                    np.sign(x)*(np.abs(x)-offset), divisor), 
+                                    sign(x)*(np.abs(x)-offset), divisor), 
                                        0.0, atol=tol) | 
                             np.isclose(
                                 np.remainder(
-                                    np.sign(x)*(np.abs(x)-offset), divisor), 
+                                    sign(x)*(np.abs(x)-offset), divisor), 
                                        divisor, atol=tol))
         if min and max:
             raise NeXusError("Select either 'min' or 'max', not both")
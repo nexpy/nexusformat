2a5f3a7d227a2bff2102a109785a10be8b39e692
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2021-11-09 16:25:41 +0000

    Refactored test_data to use fixtures

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,24 +1,58 @@
 import numpy as np
+from nexusformat.nexus.tree import NXfield, NXdata, NXroot, NXentry, NXsubentry
 import pytest
 import warnings
-from nexusformat.nexus import *
-
-x = NXfield(2 * np.linspace(0.0, 10.0, 11, dtype=np.float64), name="x")
-y = NXfield(3 * np.linspace(0.0, 5.0, 6, dtype=np.float64), name="y")
-z = NXfield(4 * np.linspace(0.0, 2.0, 3, dtype=np.float64), name="z")
-v = NXfield(np.linspace(0, 99, num=100, dtype=np.float64), name="v")
-v.resize((2, 5, 10))
-im = NXfield(np.ones(shape=(10, 10, 4), dtype=np.float32), name='image')
 
 
-def test_data_creation():
+@pytest.fixture
+def data(x, y, z, v):
+    """
+    Returns a simple NXdata object with a title.
+    """
+    return NXdata(v, (z, y, x), title="Title")
 
-    data = NXdata(v, (z, y, x), title="Title")
-    
+
+@pytest.fixture
+def data2(v):
+    """
+    Returns a different, simple NXdata object with a title.
+    """
+    v2 = v[0]
+    v2.resize((1, 5, 10))
+    return NXdata(v2)
+
+
+@pytest.fixture
+def empty_data():
+    """
+    Returns a default NXdata instance.
+    """
+    return NXdata()
+
+
+@pytest.fixture
+def NXdata_from_empty_01(empty_data, x, y, z, v):
+    empty_data.nxsignal = v
+    empty_data.nxaxes = (z, y, x)
+    return empty_data
+
+
+@pytest.fixture
+def NXdata_from_empty_02(empty_data, x, y, z, v):
+    empty_data["v"] = v
+    empty_data["x"] = x
+    empty_data["y"] = y
+    empty_data["z"] = z
+    empty_data.nxsignal = "v"
+    empty_data.nxaxes = ("z", "y", "x")
+    return empty_data
+
+
+def test_data_creation(data):
+
     assert "signal" in data.attrs
     assert "axes" in data.attrs
     assert len(data.attrs["axes"]) == 3
-    
     assert data.ndim == 3
     assert data.shape == (2, 5, 10)
     assert data.nxsignal.nxname == "v"
@@ -33,9 +67,8 @@ def test_data_creation():
     assert data.nxtitle == "Title"
 
 
-def test_default_data():
+def test_default_data(data):
 
-    data = NXdata(v, (z, y, x), title="Title")
     root = NXroot(NXentry(data))
     root["entry/data"].set_default()
 
@@ -59,9 +92,7 @@ def test_default_data():
     assert root.plottable_data is root["entry/subentry/data"]
 
 
-def test_plottable_data():
-
-    data = NXdata(v, (z, y, x), title="Title")
+def test_plottable_data_01(data):
 
     assert data.is_plottable()
     assert data.plottable_data is data
@@ -70,9 +101,8 @@ def test_plottable_data():
     assert data.plot_axes == data.nxaxes
     assert data.nxsignal.valid_axes(data.nxaxes)
 
-    v2 = v[0]
-    v2.resize((1, 5, 10))
-    data2 = NXdata(v2)
+
+def test_plottable_data_02(data2):
 
     assert data2.shape == (1, 5, 10)
     assert data2.plot_shape == (5, 10)
@@ -80,24 +110,10 @@ def test_plottable_data():
     assert data2.plot_rank == data2.nxsignal.ndim - 1
 
 
-def test_signal_selection():
-
-    data = NXdata()
-    data.nxsignal = v
-    data.nxaxes = (z, y, x)
-
-    assert data.nxsignal.nxname == "v"
-    assert [axis.nxname for axis in data.nxaxes] == ["z", "y", "x"]
-    assert np.array_equal(data.nxsignal, v)
-    assert np.array_equal(data.nxaxes[0], z)
-
-    data = NXdata()
-    data["v"] = v
-    data["x"] = x
-    data["y"] = y
-    data["z"] = z
-    data.nxsignal = "v"
-    data.nxaxes = ("z", "y", "x")
+@pytest.mark.parametrize("data",
+                         [NXdata_from_empty_01, NXdata_from_empty_02],
+                         indirect=True)
+def test_signal_selection(data, z, v):
 
     assert data.nxsignal.nxname == "v"
     assert [axis.nxname for axis in data.nxaxes] == ["z", "y", "x"]
@@ -105,12 +121,9 @@ def test_signal_selection():
     assert np.array_equal(data.nxaxes[0], z)
 
 
-def test_rename():
+def test_rename(NXdata_from_empty_01):
 
-    data = NXdata()
-    data.nxsignal = v
-    data.nxaxes = (z, y, x)
-
+    data = NXdata_from_empty_01
     data["x"].rename("xx")
     data["y"].rename("yy")
     data["z"].rename("zz")
@@ -119,11 +132,11 @@ def test_rename():
     assert [axis.nxname for axis in data.nxaxes] == ["zz", "yy", "xx"]
 
 
-def test_size_one_axis():
+def test_size_one_axis(x, z):
 
     y1 = np.array((1), dtype=np.float64)
-    v1 =  NXfield(np.linspace(0, 10*1*2, num=10*1*2, dtype=np.int64), name="v")
-    v1.resize((2,1,10))
+    v1 = NXfield(np.linspace(0, 10*1*2, num=10*1*2, dtype=np.int64), name="v")
+    v1.resize((2, 1, 10))
 
     data = NXdata(v1, (z, y1, x))
 
@@ -136,9 +149,8 @@ def test_size_one_axis():
     assert data.nxsignal.valid_axes(data.plot_axes)
 
 
-def test_data_operations():
+def test_data_operations(data, v):
 
-    data = NXdata(v, (z, y, x))
     new_data = data + 1
 
     assert np.array_equal(new_data.nxsignal.nxvalue, v + 1)
@@ -181,7 +193,7 @@ def test_data_errors():
 
     data = NXdata(v1, (y1))
     data.nxerrors = e1
-    
+
     new_data = 2 * data
 
     assert np.array_equal(new_data.nxerrors, 2 * e1)
@@ -189,11 +201,11 @@ def test_data_errors():
     new_data = 2 * data - data
 
     assert np.array_equal(new_data.nxerrors, e1 * np.sqrt(5))
- 
+
     new_data = data - data / 2
 
     assert np.array_equal(new_data.nxerrors, e1 * np.sqrt(1.25))
-   
+
 
 def test_data_weights():
 
@@ -215,7 +227,7 @@ def test_data_weights():
 
     data = NXdata(v1, (y1))
     data.nxweights = w1
-    
+
     new_data = 2 * data
 
     assert np.array_equal(new_data.nxweights, 2 * w1)
@@ -223,17 +235,15 @@ def test_data_weights():
     new_data = 2 * data - data
 
     assert np.array_equal(new_data.nxweights, w1)
- 
+
     new_data = data - data / 2
 
     assert np.array_equal(new_data.nxweights, w1/2)
-   
 
-def test_data_slabs():
 
-    data = NXdata(v, (z, y, x), title="Title")
- 
-    slab = data[0,:,:]
+def test_data_slabs(data, x, y, v):
+
+    slab = data[0, :, :]
 
     assert np.array_equal(slab.nxsignal, v[0])
     assert slab.plot_rank == 2
@@ -258,7 +268,7 @@ def test_data_slabs():
     assert slab2.shape == slab.shape
 
 
-def test_data_projections():
+def test_data_projections(x, y, z, v):
 
     d1 = NXdata(v[0], (y, x))
 
@@ -275,7 +285,7 @@ def test_data_projections():
 
     d2 = NXdata(v, (z, y, x))
 
-    p3 = d2.project((0,1),((0.,8.),(3.,9.),(4.,16.)))
+    p3 = d2.project((0, 1), ((0., 8.), (3., 9.), (4., 16.)))
 
     assert p3.nxaxes == [p3["z"], p3["y"]]
     assert np.array_equal(p3["y"].nxvalue, d2["y"][3.:9.])
@@ -284,14 +294,15 @@ def test_data_projections():
     assert p3["x"].attrs["minimum"] == 4.
     assert p3["x"].attrs["maximum"] == 16.
     assert p3["x"].attrs["summed_bins"] == 7
-    assert p3["v"].sum() == d2.v[:,1:3,2:8].sum()
-    
-    p4 = d2.project((0,1),((0.,8.),(3.,9.),(4.,16.)), summed=False)
+    assert p3["v"].sum() == d2.v[:, 1:3, 2:8].sum()
 
-    assert p4["v"].sum() == d2.v[:,1:3,2:8].sum() / p4["x"].attrs["summed_bins"]
+    p4 = d2.project((0, 1), ((0., 8.), (3., 9.), (4., 16.)), summed=False)
+
+    assert p4["v"].sum() == d2.v[:, 1:3, 2:8].sum() / \
+        p4["x"].attrs["summed_bins"]
 
 
-def test_data_smoothing():
+def test_data_smoothing(x):
 
     warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
     data = NXdata(np.sin(x), (x))
@@ -303,7 +314,7 @@ def test_data_smoothing():
     assert smooth_data.nxsignal[-1] == np.sin(x)[-1]
 
     smooth_data = data.smooth(factor=4)
-    
+
     assert smooth_data.nxsignal.shape == (41,)
     assert smooth_data.nxaxes[0].shape == (41,)
     assert smooth_data.nxsignal[0] == np.sin(x)[0]
@@ -317,27 +328,27 @@ def test_data_selection():
     yy = np.ones(shape=xx.shape, dtype=float)
     yy[np.where(np.remainder(xx, 4) == 0.0)] = 2.0
     data = NXdata(yy, xx)
-    
+
     selected_data = data.select(4.0)
 
     assert selected_data.shape == (6,)
-    assert np.all(selected_data.nxsignal==2.0)
+    assert np.all(selected_data.nxsignal == 2.0)
 
-    yy[(np.array((1,3,5,7,9,11,13,15,17,19)),)] = 1.5
+    yy[(np.array((1, 3, 5, 7, 9, 11, 13, 15, 17, 19)),)] = 1.5
     data = NXdata(yy, xx)
 
     selected_data = data.select(4.0, offset=1.0)
 
     assert selected_data.shape == (5,)
-    assert np.all(selected_data.nxsignal==1.5)
+    assert np.all(selected_data.nxsignal == 1.5)
 
     selected_data = data.select(4.0, offset=1.0, symmetric=True)
 
     assert selected_data.shape == (10,)
-    assert np.all(selected_data.nxsignal==1.5)
+    assert np.all(selected_data.nxsignal == 1.5)
 
 
-def test_image_data():
+def test_image_data(x, y, z, v, im):
 
     root = NXroot(NXentry(NXdata(im)))
     root["entry"].attrs["default"] = "data"
@@ -350,25 +361,25 @@ def test_image_data():
     assert not root["entry/other_data"].is_image()
 
 
-def test_smart_indices():
+def test_smart_indices(x, v):
 
-    ind = [1,3,5]
+    ind = [1, 3, 5]
 
     assert all(x[ind].nxvalue == x.nxvalue[ind])
-    assert all(v[v>50].nxvalue == v.nxvalue[v.nxvalue>50])
-    assert all(v[1,0,ind].nxvalue == v.nxvalue[1,0,ind])
+    assert all(v[v > 50].nxvalue == v.nxvalue[v.nxvalue > 50])
+    assert all(v[1, 0, ind].nxvalue == v.nxvalue[1, 0, ind])
 
     x[ind] = 0
 
     assert x.any() and not x[ind].any()
 
-    ind = np.array([[3, 7],[4, 5]])
+    ind = np.array([[3, 7], [4, 5]])
 
     assert np.all(x[ind].nxvalue == x.nxvalue[ind])
 
     row = np.array([0, 1, 2])
     col = np.array([2, 1, 3])
 
-    assert all(v[0][row,col].nxvalue == v[0].nxvalue[row,col])
-    assert np.all(v[0][row[:,np.newaxis],col].nxvalue == 
-                  v[0].nxvalue[row[:,np.newaxis],col])
+    assert all(v[0][row, col].nxvalue == v[0].nxvalue[row, col])
+    assert np.all(v[0][row[:, np.newaxis], col].nxvalue ==
+                  v[0].nxvalue[row[:, np.newaxis], col])

3cddfe3a163879c943bd6e9701267e554207b834
Ray Osborn <rayosborn@mac.com>
2021-11-21 17:06:34 -0600

    Updates copyright message

--- src/nexusformat/nexus/tree.py
+++ src/nexusformat/nexus/tree.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #-----------------------------------------------------------------------------
-# Copyright (c) 2013-2020, NeXpy Development Team.
+# Copyright (c) 2013-2021, NeXpy Development Team.
 #
 # Author: Paul Kienzle, Ray Osborn
 #

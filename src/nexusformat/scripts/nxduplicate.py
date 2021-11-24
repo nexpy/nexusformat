ae6d9ac03dbaf807600f848efa2c93b0f787c825
Ray Osborn <rayosborn@mac.com>
2021-11-24 16:55:03 -0600

    Remove wildcard imports

--- src/nexusformat/scripts/nxduplicate.py
+++ src/nexusformat/scripts/nxduplicate.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 #-----------------------------------------------------------------------------
-# Copyright (c) 2019-2020, NeXpy Development Team.
+# Copyright (c) 2019-2021, NeXpy Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING, distributed with this software.
 #-----------------------------------------------------------------------------
 import argparse
-from nexusformat.nexus import *
+from nexusformat.nexus import nxduplicate
 from nexusformat import __version__
+
 
 def main():
 

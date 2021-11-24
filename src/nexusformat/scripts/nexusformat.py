f41ea96e177070300054a4b839cc7f6349b1ab0d
Ray Osborn <rayosborn@mac.com>
2021-11-24 16:55:44 -0600

    Update copyright dates

--- src/nexusformat/scripts/nexusformat.py
+++ src/nexusformat/scripts/nexusformat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #-----------------------------------------------------------------------------
-# Copyright (c) 2019-2020, NeXpy Development Team.
+# Copyright (c) 2019-2021, NeXpy Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #

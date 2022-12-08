2a2475920de53126de558130edd30c11c656f437
Ray Osborn <rayosborn@mac.com>
2022-12-08 17:26:31 -0600

    Rename PylabPlotter to PyplotPlotter
    Pylab is no longer supported, so PyplotPlotter is now appropriate.

--- src/nexusformat/nexus/plot.py
+++ src/nexusformat/nexus/plot.py
@@ -96,7 +96,7 @@ def label(field):
         return field.nxname
 
 
-class PylabPlotter:
+class PyplotPlotter:
     """Matplotlib plotter class for 1D or 2D NeXus data.
 
     When the nexusformat package is used within NeXpy, plots are produced by
@@ -326,4 +326,4 @@ class PylabPlotter:
                 plt.ion()
 
 
-plotview = PylabPlotter()
+plotview = PyplotPlotter()
